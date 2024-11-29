2ba86822112b2d3407c6502f94a09e834c47b5b0
airidziox <airidas.augulis@gmail.com>
2024-11-29 10:32:05 +0200

    the project is almost done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+const body = document.querySelector('body');
+
+fetch("https://api.escuelajs.co/api/v1/users")
+.then((res) => res.json())
+.then(users => {
+
+    users.map(user => {
+        console.log(user)
+        body.innerHTML += `
+        
+        `
+    })
+})
